74ff5cb6bd61b46f8043e49a2b56d25a484a0c81
Scott <scottle220044@gmail.com>
2025-09-22 14:15:58 -0700

    implemented simple socket class

--- Networking/SimpleSocket.cpp
+++ Networking/SimpleSocket.cpp
@@ -1,1 +1,43 @@
 #include "SimpleSocket.h"
+
+
+// Default constructor for Simple socket
+SL::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface){
+    
+    // Addr structure 
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(interface);
+    
+    // connection established
+    sock = socket(domain, service, protocol);
+    test_connection(sock);
+
+    connection = establish_connection_network(sock, address);
+    test_connection(connection);
+    
+    // bind(connection, address, ) ;
+};
+
+// Test connection virtual function
+void SL::SimpleSocket::test_connection(int test_item){
+    if (test_item < 0){
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    } 
+
+};
+
+
+//Getter functions for private vals
+struct sockaddr_in SL::SimpleSocket::get_address(){
+    return address;
+}
+
+int SL::SimpleSocket::get_sock(){
+    return sock;
+}
+
+int SL::SimpleSocket::get_connection(){
+    return connection;
+}
--- Networking/SimpleSocket.h
+++ Networking/SimpleSocket.h
@@ -1,17 +1,29 @@
 #ifndef SIMPLESOCKET_H
 #define SIMPLESOCKET_H
 
+#include <iostream>
+#include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 
-namespace scope {
-    
+namespace SL {
+    // Constructor    
     class SimpleSocket{
-        public:
-            SimpleSocket();
+        private:
+            struct sockaddr_in address;
+            int sock;
+            int connection;
 
-            
-    }
+        public:
+            SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
+
+            virtual int establish_connection_network(int sock, struct sockaddr_in address) = 0;
+            void test_connection(int);
+            struct sockaddr_in get_address();
+            int get_sock();
+            int get_connection(); 
+    };
 
 }
 
