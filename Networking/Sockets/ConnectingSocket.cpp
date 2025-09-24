30e3f144cbc8e961a8ab30e7e79d3a0525fe469f
Scott <scottle220044@gmail.com>
2025-09-23 23:28:14 -0700

    Small connectingsocket fix

--- Networking/Sockets/ConnectingSocket.cpp
+++ Networking/Sockets/ConnectingSocket.cpp
@@ -2,8 +2,10 @@
 
 SL::ConnectingSocket::ConnectingSocket(int domain, int service, int protocol, int port, u_long interface):
 SimpleSocket(domain, service, protocol, port, interface){
-    set_connection(establish_connection_network(get_sock(), get_address()));
-    test_connection(get_connection());
+    // set_connection(establish_connection_network(get_sock(), get_address()));
+    // test_connection(get_connection());
+    int connection = establish_connection_network(get_sock(), get_address());
+    test_connection(connection);
 
 }   
 
